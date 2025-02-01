1ca470dee888a0ac6a5d7a6e3aa3cf25b8f9af7e
swdfgpiki8810@gmail.com <swdfgpiki8810@gmail.com>
2025-02-02 06:59:31 +0900

    finish 10.2.2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,18 +2,28 @@ module SessionsHelper
 
     def log_in(user)
         session[:user_id] = user.id 
+        # セッションリプレイ攻撃から保護する
+        # 詳しくは https://techracho.bpsinc.jp/hachi8833/2023_06_02/130443 を参照
+        session[:session_token] = user.session_token
     end
 
     def current_user
         if (user_id = session[:user_id])
-            @current_user = User.find_by(id: user_id)
-        else (user_id = cookies.encrypted[:user_id])
-            user = User.find_by(id: user_id)
-            if user && user.authenticated?(cookies[:remember_token])
-                log_in user
-                @current_user = user
-            end
+          user = User.find_by(id: user_id)
+          if user && session[:session_token] == user.session_token
+            @current_user = user
+          end
+        elsif (user_id = cookies.encrypted[:user_id])
+          user = User.find_by(id: user_id)
+          if user && user.authenticated?(cookies[:remember_token])
+            log_in user
+            @current_user = user
+          end
         end
+    end
+
+    def current_user?(user)
+        user && user == current_user
     end
 
     def logged_in?
