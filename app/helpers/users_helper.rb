df22f29b182ca56b26d1afad3ee84f16af4b065b
swdfgpiki8810@gmail.com <swdfgpiki8810@gmail.com>
2025-02-04 23:16:03 +0900

    finish 13.2.3

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,26 @@
 module UsersHelper
 
+    #無効：（画像サイズが反映されない）
     # 引数で与えられたユーザーのGravatar画像を返す
-    def gravatar_for(user, options = { size: 80 })
+    #def gravatar_for(user, size: 80)
+      #gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
+      #gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+      #image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    #end
+
+    #有効：別バージョン
+    # 引数で与えられたユーザーのGravatar画像を返す
+    #def gravatar_for(user, options = { size: 80 })
+      #size         = options[:size]
+      #gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
+      #gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+      #image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    #end
+
+    # 引数で与えられたユーザーのGravatar画像を返す
+    def gravatar_for(user, size: 80)
       gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
-      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
       image_tag(gravatar_url, alt: user.name, class: "gravatar")
     end
   end
