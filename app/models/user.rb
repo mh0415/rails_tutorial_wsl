57abeb1166a115f8c518cae02fb64598e888cc24
swdfgpiki8810@gmail.com <swdfgpiki8810@gmail.com>
2025-02-08 01:33:36 +0900

    14.1.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
     has_many :active_relationships, class_name:  "Relationship",
                                     foreign_key: "follower_id",
                                     dependent:   :destroy
-    has_many :following, through: :active_relationship, source: :followed_id
+    has_many :following, through: :active_relationships, source: :followed
     before_save { self.email = email.downcase }
     validates :name,  presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -49,6 +49,18 @@ class User < ApplicationRecord
 
     def feed
         Micropost.where("user_id = ?", id)
+    end
+
+    def follow(other_user)
+        following << other_user unless self == other_user
+    end
+
+    def unfollow(other_user)
+        following.delete(other_user)
+    end
+
+    def following?(other_user)
+        following.include?(other_user)
     end
 
 end
